6e4e6807376dc30ab287a6a2b9e38bef68c3a521
Abdou Diaw <diawa@ornl.gov>
2024-03-11 14:09:31 -0400

    all

--- gitr/gitr_material_impact_simulations.py
+++ gitr/gitr_material_impact_simulations.py
@@ -52,7 +52,7 @@ def update_species_properties(filename, impurity_species, background_species, ce
         print(f"Error updating species properties: {e}")
 
 # Testing the function
-BASE_PATH = "/Users/42d/GITR/build"
+BASE_PATH = "/Users/42d/GITR-1.3.0/build"
 OUTPUT_PATH = "/Users/42d/MPEX-WallDYN/gitr/output"
 FILENAME = "/Users/42d/MPEX-GITR-WallDYN/gitr/input/gitrInput.cfg"
 MATERIALS = [deuterium, aluminum, nitrogen, tungsten]
